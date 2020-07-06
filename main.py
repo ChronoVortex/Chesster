79d4428012e473b3252267b142bd06e932930617
MirageTD <miragetankdriver@yahoo.com>
2020-07-06 16:41:37 -0700

    roll different sided dice in the same command

--- main.py
+++ main.py
@@ -16,37 +16,56 @@ async def on_ready():
 
 # Dice roller
 @bot.command()
-async def roll(ctx, rollstr: str):
-	# Validate roll string
-	if not re.compile(r'^\d+d\d+$').match(rollstr):
-		await ctx.send('Invalid roll, enter in the format #d#.')
-		return
+async def roll(ctx, *rollstrs: str):
+	rolls = []
+	for rollstr in rollstrs:
+		# Validate roll string
+		if not re.compile(r'^\d+d\d+([+-]\d+)?$').match(rollstr):
+			await ctx.send('"{}" is not a valid roll, enter in the format #d# or #d#±#.'.format(rollstr))
+			return
+		
+		# Get roll values from string
+		roll = re.compile(r'\d+').findall(rollstr)
+		count = int(roll[0])
+		sides = int(roll[1])
+		mod = 0
+		if len(roll) > 2:
+			mod = (int(roll[2]) if rollstr[-len(roll[2])-1] == '+' else -int(roll[2]))
+		
+		# Validate roll values
+		if count < 1:
+			await ctx.send('"{}" is not a valid roll, number of dice must be greater than 0.'.format(rollstr))
+			return
+		if count > 100:
+			await ctx.send('"{}" is not a valid roll, number of dice must be less than 101.'.format(rollstr))
+			return
+		if sides < 2:
+			await ctx.send('"{}" is not a valid roll, number of sides must be greater than 1.'.format(rollstr))
+			return
+		if sides > 100:
+			await ctx.send('"{}" is not a valid roll, number of sides must be less than 101.'.format(rollstr))
+			return
+		
+		# Add roll to main list
+		rolls.append((count, sides, mod))
 	
-	# Get dice values from string
-	dice = re.compile(r'\d+').findall(rollstr)
-	count = int(dice[0])
-	sides = int(dice[1])
+	# Calculate roll(s)
+	roll_list = []
+	mods = []
+	for count, sides, mod in rolls:
+		for _ in range(count):
+			roll_list.append(randrange(1, sides + 1))
+		if not mod == 0:
+			mods.append(mod)
 	
-	# Validate dice values
-	if count < 1:
-		await ctx.send('Invalid roll, number of dice must be greater than 0.')
-		return
-	if count > 100:
-		await ctx.send('Invalid roll, number of dice must be less than 101.')
-		return
-	if sides < 2:
-		await ctx.send('Invalid roll, number of sides must be greater than 1.')
-		return
-	if sides > 100:
-		await ctx.send('Invalid roll, number of sides must be less than 101.')
-		return
-	
-	# Calculate roll and send results
-	if count == 1:
-		await ctx.send('Result: {}'.format(randrange(1, sides + 1)))
+	# Send results
+	if len(roll_list) == 1:
+		await ctx.send('Result: {}'.format(roll_list[0] + sum(mods)))
 	else:
-		roll_list = [randrange(1, sides + 1) for n in range(count)]
-		await ctx.send('Results: {}\nTotal: {}'.format(', '.join(map(str, roll_list)), sum(roll_list)))
+		to_send = 'Results: {}\n'.format(', '.join(map(str, roll_list)))
+		if len(mods) > 0:
+			to_send += 'Modifiers: {}\n'.format(', '.join(map(str, mods)))
+		await ctx.send('{}Total: {}'.format(to_send, sum(roll_list) + sum(mods)))
 
 # Load cogs
 cogs = ['cogs.chess']
