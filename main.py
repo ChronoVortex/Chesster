565407c2b864b6016a7d310c048d2051dc71d4c5
MirageTD <miragetankdriver@yahoo.com>
2020-07-13 17:06:16 -0700

    removed tabs, improved qrng caching

--- main.py
+++ main.py
@@ -9,87 +9,89 @@ import settings
 import os
 import re
 if settings.use_quantum_rng:
-	print('Initializing with QRNG')
-	from qrandom import randrange
+    print('Initializing with QRNG')
+    from qrandom import Qrandom
+    qrandom_inst = Qrandom()
+    randrange = qrandom_inst.randint
 else:
-	print('Initializing with PRNG')
-	from random import randrange
+    print('Initializing with PRNG')
+    from random import randrange
 
 # Initilize the bot
 bot = commands.Bot(command_prefix = '/')
 
 @bot.event
 async def on_ready():
-	print('Logged in as {0.user}'.format(bot))
+    print('Logged in as {0.user}'.format(bot))
 
 # Dice roller
 @bot.command()
 async def roll(ctx, *rollstrs: str):
-	rolls = []
-	for rollstr in rollstrs:
-		# Validate roll string
-		if not re.compile(r'^\d+d\d+([+-]\d+)?$').match(rollstr):
-			await ctx.send('"{}" is not a valid roll, enter in the format #d# or #d#±#.'.format(rollstr))
-			return
-		
-		# Get roll values from string
-		roll = re.compile(r'\d+').findall(rollstr)
-		count = int(roll[0])
-		sides = int(roll[1])
-		mod = 0
-		if len(roll) > 2:
-			mod = (int(roll[2]) if rollstr[-len(roll[2])-1] == '+' else -int(roll[2]))
-		
-		# Validate roll values
-		if count < 1:
-			await ctx.send('"{}" is not a valid roll, number of dice must be greater than 0.'.format(rollstr))
-			return
-		if count > 100:
-			await ctx.send('"{}" is not a valid roll, number of dice must be less than 101.'.format(rollstr))
-			return
-		if sides < 2:
-			await ctx.send('"{}" is not a valid roll, number of sides must be greater than 1.'.format(rollstr))
-			return
-		if sides > 100:
-			await ctx.send('"{}" is not a valid roll, number of sides must be less than 101.'.format(rollstr))
-			return
-		
-		# Add roll to main list
-		rolls.append((count, sides, mod))
-	
-	# Calculate roll(s)
-	roll_list = []
-	mods = []
-	for count, sides, mod in rolls:
-		for _ in range(count):
-			roll_list.append(randrange(1, sides + 1))
-		if not mod == 0:
-			mods.append(mod)
-	
-	# Send results
-	if len(roll_list) == 1:
-		await ctx.send('Result: {}'.format(roll_list[0] + sum(mods)))
-	else:
-		to_send = 'Results: {}\n'.format(', '.join(map(str, roll_list)))
-		if len(mods) > 0:
-			to_send += 'Modifiers: {}\n'.format(', '.join(map(str, mods)))
-		await ctx.send('{}Total: {}'.format(to_send, sum(roll_list) + sum(mods)))
+    rolls = []
+    for rollstr in rollstrs:
+        # Validate roll string
+        if not re.compile(r'^\d+d\d+([+-]\d+)?$').match(rollstr):
+            await ctx.send('"{}" is not a valid roll, enter in the format #d# or #d#±#.'.format(rollstr))
+            return
+        
+        # Get roll values from string
+        roll = re.compile(r'\d+').findall(rollstr)
+        count = int(roll[0])
+        sides = int(roll[1])
+        mod = 0
+        if len(roll) > 2:
+            mod = (int(roll[2]) if rollstr[-len(roll[2])-1] == '+' else -int(roll[2]))
+        
+        # Validate roll values
+        if count < 1:
+            await ctx.send('"{}" is not a valid roll, number of dice must be greater than 0.'.format(rollstr))
+            return
+        if count > 100:
+            await ctx.send('"{}" is not a valid roll, number of dice must be less than 101.'.format(rollstr))
+            return
+        if sides < 2:
+            await ctx.send('"{}" is not a valid roll, number of sides must be greater than 1.'.format(rollstr))
+            return
+        if sides > 100:
+            await ctx.send('"{}" is not a valid roll, number of sides must be less than 101.'.format(rollstr))
+            return
+        
+        # Add roll to main list
+        rolls.append((count, sides, mod))
+    
+    # Calculate roll(s)
+    roll_list = []
+    mods = []
+    for count, sides, mod in rolls:
+        for _ in range(count):
+            roll_list.append(randrange(1, sides + 1))
+        if not mod == 0:
+            mods.append(mod)
+    
+    # Send results
+    if len(roll_list) == 1:
+        await ctx.send('Result: {}'.format(roll_list[0] + sum(mods)))
+    else:
+        to_send = 'Results: {}\n'.format(', '.join(map(str, roll_list)))
+        if len(mods) > 0:
+            to_send += 'Modifiers: {}\n'.format(', '.join(map(str, mods)))
+        await ctx.send('{}Total: {}'.format(to_send, sum(roll_list) + sum(mods)))
 
 # Load cogs
 cogs = ['cogs.chess']
 for cog in cogs:
-	try:
-		bot.load_extension(cog)
-	except Exception:
-		print('Unable to load cog {}'.format(cog))
+    try:
+        bot.load_extension(cog)
+    except Exception:
+        print('Unable to load cog {}'.format(cog))
 
 # Run bot
 token = ''
 if os.path.isfile('./token'):
-	with open('token', 'r') as f:
-		token = f.readline()
+    with open('token', 'r') as f:
+        token = f.readline()
 else:
-	token = str(input('Enter bot token: '))
-	with open('token', 'w') as f:
-		f.write(token)
+    token = str(input('Enter bot token: '))
+    with open('token', 'w') as f:
+        f.write(token)
 bot.run(token)
--- qrandom.py
+++ qrandom.py
@@ -6,58 +6,70 @@ https://pypi.org/project/quantumrandom/
 import math
 import requests
 try:
-	import json
+    import json
 except ImportError:
-	import simplejson as json
+    import simplejson as json
 
 DATA_TYPES = ['uint16', 'hex16']
 MAX_LEN = 1024
 INT_BITS = 16
 
-def get_data(data_type='uint16', array_length=1, block_size=1):
-	"""Fetch data from the ANU Quantum Random Numbers JSON API"""
-	if data_type not in DATA_TYPES:
-		raise Exception("data_type must be one of %s" % DATA_TYPES)
-	if array_length > MAX_LEN:
-		raise Exception("array_length cannot be larger than %s" % MAX_LEN)
-	if block_size > MAX_LEN:
-		raise Exception("block_size cannot be larger than %s" % MAX_LEN)
-	url = 'https://qrng.anu.edu.au/API/jsonI.php?length={}&type={}&size={}'.format(
-		array_length, data_type, block_size)
-	data = json.loads(requests.get(url).content.decode())
-	assert data['success'] is True, data
-	assert data['length'] == array_length, data
-	return data['data']
+class Qrandom():
+    def __init__(self):
+        self.cached_generator = self.qrand_generator()
+    
+    def get_data(self, data_type='uint16', array_length=1, block_size=1):
+        """Fetch data from the ANU Quantum Random Numbers JSON API."""
+        print('Requesting data from qrng.anu.edu.au')
+        if data_type not in DATA_TYPES:
+            raise Exception("data_type must be one of %s" % DATA_TYPES)
+        if array_length > MAX_LEN:
+            raise Exception("array_length cannot be larger than %s" % MAX_LEN)
+        if block_size > MAX_LEN:
+            raise Exception("block_size cannot be larger than %s" % MAX_LEN)
+        url = 'https://qrng.anu.edu.au/API/jsonI.php?length={}&type={}&size={}'.format(
+            array_length, data_type, block_size)
+        data = json.loads(requests.get(url).content.decode())
+        assert data['success'] is True, data
+        assert data['length'] == array_length, data
+        return data['data']
 
-def cached_generator(data_type='uint16', cache_size=1024):
-	"""Returns numbers. Caches numbers to avoid latency."""
-	while 1:
-		for n in get_data(data_type, cache_size, cache_size):
-			yield n
-
-def randrange(min=0, max=10, generator=None):
-	"""Return an int between min and max. If given, takes from generator instead.
-	This can be useful to reuse the same cached_generator() instance over multiple calls."""
-	rand_range = max - min
-	if rand_range == 0:
-		# raise ValueError("range cannot be zero")  # meh
-		return min
-
-	if generator is None:
-		generator = cached_generator()
-
-	source_bits = int(math.ceil(math.log(rand_range + 1, 2)))
-	source_size = int(math.ceil(source_bits / float(INT_BITS)))
-	source_max = 2 ** (source_size * INT_BITS) - 1
-
-	modulos = source_max / rand_range
-	too_big = modulos * rand_range
-	while True:
-		num = 0
-		for x in range(source_size):
-			num <<= INT_BITS
-			num += next(generator)
-		if num >= too_big:
-			continue
-		else:
-			return int(num//modulos + min)
+    def qrand_generator(self, data_type='uint16', cache_size=MAX_LEN):
+        """Generator for numbers. Caches numbers to avoid latency."""
+        while 1:
+            for n in self.get_data(data_type, cache_size, cache_size):
+                yield n
+    
+    def qrng_next(self):
+        """Return cached numbers."""
+        res = next(self.cached_generator, None)
+        if res == None: # Old generator is empty, create a new one
+            self.cached_generator = self.qrand_generator()
+            return next(self.cached_generator)
+        else:
+            return res
+    
+    def randint(self, min: int, max: int):
+        """Return an int between min and max. If given, takes from generator instead.
+        This can be useful to reuse the same qrand_generator() instance over multiple calls."""
+        rand_range = max - min
+        if rand_range < 0:
+            raise ValueError('max is less than min.')
+        if rand_range == 0:
+            return min
+        
+        source_bits = int(math.ceil(math.log(rand_range + 1, 2)))
+        source_size = int(math.ceil(source_bits / float(INT_BITS)))
+        source_max = 2 ** (source_size * INT_BITS) - 1
+        
+        modulos = source_max / rand_range
+        too_big = modulos * rand_range
+        while True:
+            num = 0
+            for x in range(source_size):
+                num <<= INT_BITS
+                num += self.qrng_next()
+            if num >= too_big:
+                continue
+            else:
+                return int(math.floor(num / modulos) + min)
